55e3bc388dc6e855acef3bd97f3a4c6cf388439a
lruiabc <568047669@qq.om>
2015-11-19 21:56:00 -0600

    change the address

--- install-webserver.sh
+++ install-webserver.sh
@@ -5,10 +5,10 @@ sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/lruiabc/itmo544-mp2.git
 
-mv ./../itmo544-mp1/images /var/www/html/images
-mv ./../itmo544-mp1/vendor /var/www/html/vendor
-mv ./../itmo544-mp1/index.html /var/www/html
-mv ./../itmo544-mp1/*.php /var/www/html
+mv ./../itmo544-mp2/images /var/www/html/images
+mv ./../itmo544-mp2/vendor /var/www/html/vendor
+mv ./../itmo544-mp2/index.html /var/www/html
+mv ./../itmo544-mp2/*.php /var/www/html
 
 curl -sS http://getcomposer.org/installer | php
 php composer.phar require aws/aws-sdk-php
