2a302607ef492b4937788b75f679bd09bff76340
lruiabc <568047669@qq.om>
2015-11-23 17:05:59 -0600

    added a new test.sh

--- test.sh
+++ test.sh
@@ -56,8 +56,8 @@ aws cloudwatch put-metric-alarm --alarm-name cpu-mi --alarm-description "Alarm w
 #     done
 #fi
 #sns service
-ARN=('aws sns creat-topic --name mp2')
+ARN=('aws sns create-topic --name mp2')
 echo "this is the ARN:$ARN"
 aws sns set-topic-attributes --topic-arn $attribute-name displayName --attribute-value mp2
-aws sns subcribe --topic-arn $ARN  --protocol email --notification-endpoint lrui@hawk.iit.edu
+aws sns subscribe --topic-arn $ARN  --protocol email --notification-endpoint lrui@hawk.iit.edu
 aws sns publish --topic-arn $ARN --message "best"
