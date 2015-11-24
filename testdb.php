1703a1dc2d207a20bd392c1e687ba66c3b8161fa
lruiabc <568047669@qq.om>
2015-11-23 20:50:08 -0600

    added testdb.php

--- testdb.php
+++ testdb.php
@@ -5,32 +5,15 @@ $client = RdsClient::factory(array(
 'version' =>'latest',
 'region'  => 'us-west-2'
 ));
-//$result = $client->describeDBInstances(array(
-  //  'DBInstanceIdentifier' => 'itmo544-ght-db',
-//));
-//$endpoint = ""; 
-//foreach ($result->getPath('DBInstances/*/Endpoint/Address') as $ep) {
-    // Do something with the message
-  //  echo "============". $ep . "================";
-    //$endpoint = $ep;
-//}
+
 echo "begin database";
 $link = mysqli_connect("itmo544grh-mp1.ckh0wwv6itjw.us-west-2.rds.amazonaws.com","rui","110224Fish","itmoruidb") or die("Error " . mysqli_error($link));
-/* check connection */
+
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-/*
-$delete_table = 'DELETE TABLE student';
-$del_tbl = $link->query($delete_table);
-if ($delete_table) {
-        echo "Table student has been deleted";
-}
-else {
-        echo "error!!";
-}
-*/
+
 $create_table = 'CREATE TABLE comments IF NOT EXISTS items  
 (
     id INT NOT NULL AUTO_INCREMENT,
