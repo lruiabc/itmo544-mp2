1ec2acd1f661b65e021893d0028ac5c949803c3a
lruiabc <568047669@qq.om>
2015-11-29 21:47:14 -0600

    added a new install.webservser.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,13 +3,13 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
-git clone https://github.com/lruiabc/itmo544-mp2.git
+git clone https://github.com/lruiabc/itmo544mp1.git
 
-mv ./../itmo544-mp2/images /var/www/html/images
-mv ./../itmo544-mp2/vendor /var/www/html/vendor
-mv ./../itmo544-mp2/index.html /var/www/html
-mv ./../itmo544-mp2/*.php /var/www/html
-mv ./../itmo544-mp2/*.asp /var/www/html
+mv ./../itmo544mp1/images /var/www/html/images
+mv ./../itmo544mp1/vendor /var/www/html/vendor
+mv ./../itmo544mp1/index.html /var/www/html
+mv ./../itmo544mp1/*.php /var/www/html
+
 
 curl -sS http://getcomposer.org/installer | php
 php composer.phar require aws/aws-sdk-php
@@ -17,9 +17,7 @@ php composer.phar require aws/aws-sdk-php
 #echo -e "\nSleeping 5 seconds"
 #for i in {0..5}; do echo -ne '.';sleep 1;done
 
-sudo mv ./../itmo544-mp2/vendor /var/www/html/vendor
-
-sudo php /var/www/html/setup.php
+sudo mv ./../itmo544mp1/vendor /var/www/html/vendor
 
 echo "hello!" >/tmp/hello.txt
 
