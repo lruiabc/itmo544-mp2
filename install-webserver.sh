30e562e2169cca7b23ccbd833d939245ba3877bd
lruiabc <568047669@qq.om>
2015-11-29 22:52:09 -0600

    added a new stall-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -5,10 +5,10 @@ sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/lruiabc/itmo544mp1.git
 
-mv ./../itmo544mp1/images /var/www/html/images
-mv ./../itmo544mp1/vendor /var/www/html/vendor
-mv ./../itmo544mp1/index.html /var/www/html
-mv ./../itmo544mp1/*.php /var/www/html
+mv ./../itmo544-mp2/images /var/www/html/images
+mv ./../itmo544-mp2/vendor /var/www/html/vendor
+mv ./../itmo544-mp2/index.html /var/www/html
+mv ./../itmo544-mp2/*.php /var/www/html
 
 
 curl -sS http://getcomposer.org/installer | php
@@ -16,7 +16,7 @@ sudo php composer.phar require aws/aws-sdk-php
 
 
 
-sudo mv ./../itmo544mp1/vendor /var/www/html/vendor
+sudo mv ./vendor /var/www/html/vendor
 
 echo "hello!" >/tmp/hello.txt
 
